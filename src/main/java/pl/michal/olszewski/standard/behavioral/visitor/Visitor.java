9da979d748a5ef3f399af0b3357ae9d7f1353feb
Michał <olszewskimichal@outlook.com>
2017-12-06 20:37:36 +0100

    Dodanie komentarza do czego słuzy i jakie są wady i zalety

--- src/main/java/pl/michal/olszewski/standard/behavioral/visitor/Visitor.java
+++ src/main/java/pl/michal/olszewski/standard/behavioral/visitor/Visitor.java
@@ -1,5 +1,22 @@
 package pl.michal.olszewski.standard.behavioral.visitor;
 
+/**
+ *
+ * Zadaniem wzorca projektowego Visitor jest oddzielenie algorytmu od struktury obiektów, na
+ * których operuje. W opisywanym wzorcu projektowym wprowadzony zostaje nowy obiekt Visitor,
+ * którego zadaniem jest odwiedzenie każdego z obiektów pewnego zespołu i wykonanie na każdym z
+ * nich konkretnej operacji
+ *
+ * Uzywamy np by uniknać ifologii z instanceOf
+ *
+ * Wady
+ * --Dodanie nowego elementu, który ma być obsługiwany przez wzorzec wymusza dodanie implementacji obsługi do wszystkich istniejących odwiedzających.
+ * -- Każdy odwiedzający musi mieć dostęp do składowych elementu co oznacza, że każdy element musi mieć publiczny dostęp do używanych składowych obiektu.
+ *
+ * Zalety
+ * -- Wymusza dodanie obsługi implementacji do wszystkich istniejących odwiedzających zapewniając tym samym obsługę elementu przez wszystkie zadania.
+ * -- Grupuje funkcjonalność logicznie powiązaną ze sobą w jedną klasę przez co dużo łatwiej jest ją rozwijać i utrzymywać.
+ */
 public interface Visitor<T> {
 
   T visit(Foo foo);
