9f52dfb1b7c019789c25eb3e14930eb2cd9ced2f
Michał <olszewskimichal@outlook.com>
2017-12-07 17:35:22 +0100

    wzorzec composite - kompozyt

---
+++ src/main/java/pl/michal/olszewski/standard/design/composite/Employee.java
@@ -0,0 +1,17 @@
+package pl.michal.olszewski.standard.design.composite;
+
+import java.math.BigDecimal;
+
+/**
+ * Component
+ *
+ * -- declares interface for objects in composition.
+ * -- implements deafault behaviour for the interface common to all classes as appropriate.
+ * -- declares an interface for accessing and managing its child components.
+ */
+public interface Employee {
+  String getName();
+  BigDecimal getSalary();
+  void print();
+  void addEmployee(Employee e);
+}
---
+++ src/main/java/pl/michal/olszewski/standard/design/composite/Manager.java
@@ -0,0 +1,43 @@
+package pl.michal.olszewski.standard.design.composite;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Composite
+ *
+ * Wzorzec projektowy Composite jest strukturalnym wzorcem projektowym wykorzystywanym przy tworzeniu hierarchiczne struktury (drzewiastej) złożonej z obietków. Pozwala traktować jednakowo obiekty jak i grupy obiektów.
+ */
+public class Manager implements Employee {
+
+  private final String name;
+  private final BigDecimal salary;
+  private List<Employee> employees = new ArrayList<>();
+
+  public Manager(String name, BigDecimal salary) {
+    this.name = name;
+    this.salary = salary;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public BigDecimal getSalary() {
+    BigDecimal allSalary = employees.stream().map(Employee::getSalary).reduce(BigDecimal.ZERO, BigDecimal::add);
+    return salary.add(allSalary);
+  }
+
+  public void addEmployee(Employee e) {
+    employees.add(e);
+  }
+
+  @Override
+  public void print() {
+    System.out.println(getName() + " " + getSalary());
+    employees.forEach(Employee::print);
+  }
+}
