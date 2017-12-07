02bdc305a9e5f2de5f6cc874416602a41259a12b
Michał <olszewskimichal@outlook.com>
2017-12-07 22:30:56 +0100

    Zmiana nazwy klasy testowej

--- src/test/java/pl/michal/olszewski/standard/design/chainResponsibility/ChainOfResponsibilityTest.java
+++ src/test/java/pl/michal/olszewski/standard/design/chainResponsibility/ChainOfResponsibilityTest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class DispenserTest {
+class ChainOfResponsibilityTest {
 
   private DispenseChain dispenseChain;
 
