57528f8c2ed83bf764cece771d758b61a7c17e80
Michał <olszewskimichal@outlook.com>
2017-12-07 18:52:02 +0100

    Opis wzorca

--- src/main/java/pl/michal/olszewski/standard/design/chainResponsibility/DispenseChain.java
+++ src/main/java/pl/michal/olszewski/standard/design/chainResponsibility/DispenseChain.java
@@ -4,6 +4,19 @@ import static java.lang.System.out;
 
 import java.math.BigDecimal;
 
+/**
+ * Chain of Responsibility
+ *
+ * nazywany jest również łańcuchem zobowiązań, ponieważ jego głównym zadaniem  jest przekazanie zobowiązania obsługi żądania na kolejny element w łańcuchu. Łańcuch takich obiektów tworzy listę jednokierunkową, w której odpowiedzialność za przetworzone żądanie spada na najlepiej przygotowany do tego obiekt.
+ *
+ * Zalety:
+ * - klient, przekazując żądanie, nie wie, który z obiektów będzie je w rzeczywistości obsługiwał
+ * - możliwość elastycznego przydziału odpowiedzialności do obiektów
+ *
+ * Wady:
+ * - brak gwarancji obsługi żądania: kolejne ogniwa mogą zrezygnować z zajęcia się nim
+ *
+ */
 public interface DispenseChain {
 
   BigDecimal getDispenseValue();
