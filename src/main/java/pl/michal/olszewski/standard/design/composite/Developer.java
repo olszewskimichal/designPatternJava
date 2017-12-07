daf869764f0b0d85bfc5e3156ab093e1e4f7ac6a
Michał <olszewskimichal@outlook.com>
2017-12-07 17:35:22 +0100

    wzorzec composite - kompozyt

---
+++ src/main/java/pl/michal/olszewski/standard/design/composite/Developer.java
@@ -0,0 +1,39 @@
+package pl.michal.olszewski.standard.design.composite;
+
+import java.math.BigDecimal;
+
+/**
+ * Leaf
+ *
+ * - typ prosty - nie posiada potomków.
+ */
+public class Developer implements Employee {
+
+  private final String name;
+  private final BigDecimal salary;
+
+  public Developer(String name, BigDecimal salary) {
+    this.name = name;
+    this.salary = salary;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public BigDecimal getSalary() {
+    return salary;
+  }
+
+  @Override
+  public void print() {
+    System.out.println(getName() + " " + getSalary());
+  }
+
+  @Override
+  public void addEmployee(Employee e) {
+    throw new IllegalStateException("Developer nie moze miec pracowinika pod sobą");
+  }
+}
---
+++ src/test/java/pl/michal/olszewski/standard/design/composite/CompositeTest.java
@@ -0,0 +1,32 @@
+package pl.michal.olszewski.standard.design.composite;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Klient– operuje na danych zawartych we wzorcu
+ */
+class CompositeTest {
+
+  @Test
+  void shouldReturnAllSalary() {
+    Employee employee = new Developer("Aaa", BigDecimal.ONE);
+    Employee employee2 = new Developer("Bbb", BigDecimal.TEN);
+    Employee manager = new Manager("Ccc", BigDecimal.ZERO);
+    manager.addEmployee(employee);
+    manager.addEmployee(employee2);
+    Employee generalManager = new Manager("Ddd", BigDecimal.valueOf(15));
+    generalManager.addEmployee(manager);
+
+    assertAll(
+        () -> assertThat(employee.getSalary()).isEqualTo(BigDecimal.ONE),
+        () -> assertThat(manager.getSalary()).isEqualTo(BigDecimal.valueOf(11)),
+        () -> assertThat(generalManager.getSalary()).isEqualTo(BigDecimal.valueOf(26)),
+        () -> assertThrows(IllegalStateException.class, () -> employee.addEmployee(employee2))
+    );
+  }
+}
