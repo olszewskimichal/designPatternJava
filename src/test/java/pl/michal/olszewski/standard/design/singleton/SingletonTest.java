f2492f5ea1c2ecc6a6cec411debbc37a6db8ca63
Michał <olszewskimichal@outlook.com>
2017-12-10 20:41:53 +0100

    komentarz do singletona

--- src/test/java/pl/michal/olszewski/standard/design/singleton/SingletonTest.java
+++ src/test/java/pl/michal/olszewski/standard/design/singleton/SingletonTest.java
@@ -3,8 +3,19 @@ package pl.michal.olszewski.standard.design.singleton;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.junit.jupiter.api.Test;
-import pl.michal.olszewski.standard.design.singleton.ClassSingleton;
+/*
+Zalety:
 
+Pobieranie instancji klasy jest niewidoczne dla użytkownika. Nie musi on wiedzieć, czy w chwili wywołania metody instancja istnieje czy dopiero jest tworzona.
+Tworzenie nowej instancji zachodzi dopiero przy pierwszej próbie użycia.
+Klasa zaimplementowana z użyciem wzorca singleton może samodzielnie kontrolować liczbę swoich instancji istniejących w aplikacji.
+Wady:
+
+Brak elastyczności, ponieważ już na poziomie kodu, na „sztywno” określana jest liczba instancji klasy.
+Utrudnia testowanie i usuwanie błędów w aplikacji. Poważnie utrudnia testowanie aplikacji przez wprowadzenie do niej globalnego stanu
+
+
+ */
 class SingletonTest {
 
   @Test
